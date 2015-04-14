c8a217a5dcada9de706286062015b0d0d7dd7588
Andrew <info@petabyet.com>
2015-04-14 14:05:51 +1000

    Typo

--- config.php
+++ config.php
@@ -57,7 +57,7 @@ $config['logintimeout']     = '1800';                   //inactivity timeout in 
 $config['debug']            = false;                    //debug mode
 $config['debuglevel']       = 2;                        //0 for just show errors, 1 for just log errors or 2 for show and log errors
 $config['errorlevels']      = 'E_ALL | E_STRICT';       //error reporting level
-$config['logerrors']        = true;                     //log errors to the syslog even when degub is set to false
+$config['logerrors']        = true;                     //log errors to the syslog even when debug is set to false
 $config['timezone']         = 'UTC';                    //default time zone to use
 
 // DON'T EDIT BELOW THIS LINE
